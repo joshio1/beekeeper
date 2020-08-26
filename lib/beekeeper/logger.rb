456b563f217b7b44e9a768a40336d4ef1fc40f0e
Omkar Joshi <onjoshi@ncsu.edu>
2020-08-26 10:39:48 +0530

    Used logger in rescuer

--- lib/beekeeper/logger.rb
+++ lib/beekeeper/logger.rb
@@ -1,12 +1,37 @@
 module Beekeeper
   class Logger
     def self.fatal(error)
+      message = if error.respond_to? :backtrace
+                  message_with_backtrace(error)
+                else
+                  error
+                end
+      Rails.logger.fatal message
+    end
+
+    def self.error(error)
+      message = if error.respond_to? :backtrace
+                  message_with_backtrace(error)
+                else
+                  error
+                end
+      Rails.logger.error message
+    end
+
+    private
+
+    def self.message_with_backtrace(error)
+      trace = clean(error.backtrace)
+      "\n#{error.class.name} (#{error.message}):\n#{trace.join}"
+    end
+
+    def self.clean(backtrace)
       bc = ActiveSupport::BacktraceCleaner.new
       bc.add_filter   { |line| line.gsub(Rails.root.to_s, '') } # strip the Rails.root prefix
       bc.add_silencer { |line| line =~ /puma|rubygems|vendor|bin/ } # skip any lines from puma or rubygems
-      trace = bc.clean(error.backtrace)
-      Rails.logger.fatal "\n#{error.class.name} (#{error.message}):\n#{trace.join}"
+      bc.clean(backtrace).map! { |t| "  #{t}\n" }
     end
+
   end
 end
 
--- lib/beekeeper/rescuer.rb
+++ lib/beekeeper/rescuer.rb
@@ -13,16 +13,9 @@ module Beekeeper
       rescue_from(*RESCUABLE_ERRORS, with: :handle_error)
 
       def handle_error(error)
-        log(error)
+        Logger.error(error)
         response = ExceptionBlueprinter.render_as_hash error, view: :basic, request: request
         render json: response, status: response[:status]
-      end
-
-      def log(error)
-        backtrace_cleaner = request.env["action_dispatch.backtrace_cleaner"]
-        application_trace = ActionDispatch::ExceptionWrapper.new(backtrace_cleaner, error).application_trace
-        application_trace.map! { |t| "  #{t}\n" }
-        Rails.logger.error "\n#{error.class.name} (#{error.message}):\n#{application_trace.join}"
       end
 
     end
